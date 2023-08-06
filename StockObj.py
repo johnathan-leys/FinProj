b62b2c366a6ff891ebfb5aa4d64ec483827604b1
Jack Leys <jackal92591@gmail.com>
2023-08-06 14:27:48 -0400

    More oop functions

--- StockObj.py
+++ StockObj.py
@@ -54,6 +54,43 @@ class StockData:
         mpf.plot(self.data, type=chart_type, title=f'{self.symbol} {chart_type.capitalize()} Chart',
              ylabel='Price', datetime_format='%H:%M', xrotation=45, **kwargs)
 
+    def calculate_volatility(self, window= 15):
+        # Calculate returns
+        self.data['ReturnsPct'] = self.data['Close'].pct_change()
+
+        # Calculate volatility
+        self.data['Volatility'] = self.data['ReturnsPct'].rolling(window=window).std() * np.sqrt(window)
+
+        return self.data[['Close', 'Volatility']]  # Return just the needed columns
+
+    # Calculate and add RSI field to data
+    def calculate_rsi(self, window=10):
+        # Calculate daily returns, gains/losses
+        self.data['Returns'] = self.data['Close'].diff()
+        self.data['Gains'] = np.where(self.data['Returns'] > 0, self.data['Returns'], 0)
+        self.data['Losses'] = np.where(self.data['Returns'] < 0, abs(self.data['Returns']), 0)
+
+        # Calculate average gains and losses
+        avg_gains = self.data['Gains'].rolling(window=window).mean()
+        avg_losses = self.data['Losses'].rolling(window=window).mean()
+
+        # Calculate RSI
+        rs = avg_gains / avg_losses
+        rsi = 100 - (100 / (1 + rs))
+        self.data['RSI'] = rsi
+
+        # Remove the unneeded gains/losses columns
+        self.data.drop('Gains', axis=1, inplace=True)
+        self.data.drop('Losses', axis=1, inplace=True)
+
+        return self.data[['Close', 'RSI']] # Return just the RSI fields
+
+    def df_to_csv(self, filename='AllData.csv'):
+        if(filename == 'AllData.csv'):
+            self.data.to_csv('DataFiles/' + self.symbol + 'AllData.csv')
+        else:
+            self.data.to_csv('DataFiles/' + filename)
+
 
 if __name__ == '__main__':
     # If you clone this repo, be sure to update the location of your own API key
@@ -64,9 +101,15 @@ if __name__ == '__main__':
     
     Apple_Obj = StockData(API_KEY, stock_symbol)
     Apple_Obj.get_stock_data()
-    print(Apple_Obj.data)
+   
     Apple_Obj.plot_prices()
     Apple_Obj.plot_mplfinance(style='nightclouds', mav=(5, 20), volume=True)
+    Apple_Obj.calculate_volatility()
+    Apple_Obj.calculate_rsi()
+
+    Apple_Obj.df_to_csv()
+
+    print(Apple_Obj.data)
 
 
 
