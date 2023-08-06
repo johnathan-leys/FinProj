f34dad2f761138c680308655a03899d76f6ef249
Jack Leys <jackal92591@gmail.com>
2023-08-05 20:33:18 -0400

    updated default ticker

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from stockFunc import *
 
-stock_symbol = 'TSLA'  # Test stock symbol
+stock_symbol = 'SPY'  # Test stock symbol
 interval = '1min'  # Options are: '5min', '15min', '30min', or '60min'
 
 stock_data = get_stock_data(stock_symbol, interval)
 daily_data = get_stock_data_daily(stock_symbol)
 
-plot_mplfinance(stock_data, 'TSLA', style='nightclouds', mav=(5, 20), volume=True)
+plot_mplfinance(stock_data, stock_symbol, style='nightclouds', mav=(5, 20), volume=True)
 
 if not os.path.exists('DataFiles'):
      os.makedirs('DataFiles')
