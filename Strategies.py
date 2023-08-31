9889f8f96b2483277f78320ab122ed220bff043a
Jack Leys <jackal92591@gmail.com>
2023-08-30 20:10:15 -0400

    basic strats added

--- Strategies.py
+++ Strategies.py
@@ -1,12 +1,35 @@
 #   This file contains more complex trading strategies than the simple ones built into the StockData
-#   class. The strategies typically still modify the data in the base StockData class
+#   class. The strategies typically still modify the data in the base StockData class.
 
 from StockObj import *
 
 import pandas_ta as ta
 
 #   Define strategies here:
-AllStrategy = ta.AllStrategy
+All_Strategy = ta.AllStrategy   # Requires TA-Lib
+
+CustomStrategy = ta.Strategy(   # Example from pandas-ta README
+    name="Momo and Volatility",
+    description="SMA 50,200, BBANDS, RSI, MACD and Volume SMA 20",
+    ta=[
+        {"kind": "sma", "length": 50},
+        {"kind": "sma", "length": 200},
+        {"kind": "bbands", "length": 20},
+        {"kind": "rsi"},
+        {"kind": "macd", "fast": 8, "slow": 21},
+        {"kind": "sma", "close": "volume", "length": 20, "prefix": "VOLUME"},
+    ]
+)
+
+obv_vwma50_strat = ta.Strategy(   # 
+    name="OBV, VWMA",
+    description="On balance volume and volume-weighted moving average",
+    ta = [
+            {"kind": "obv"},
+            {"kind": "vwma", "length":50, "prefix": "VWMA_{}".format(50)}
+        ]
+)
+
 
 #   Might not even want this class, can add function to base and just include the strategies file
 #   ---------------------------------------------------------------------------------------------
@@ -17,6 +40,8 @@ class Strategies(StockData):
         self.data.ta.strategy(strategy)
 
 if __name__ == '__main__':
+
+    
     # If you clone this repo, be sure to update the location of your own API key
     with open(".APIkeys", 'r') as file:
         API_KEY = file.readline().strip().split()[0]
@@ -24,7 +49,10 @@ if __name__ == '__main__':
     stock_symbol = 'NVDA'  
     NVDA = Strategies(API_KEY, stock_symbol)
     NVDA.fetch_stock_data()
-    NVDA.execute_strategy()
+
+    print(NVDA.data)
+    NVDA.execute_strategy(obv_vwma50_strat)
+    NVDA.execute_strategy(CustomStrategy)
 
     print(NVDA.data)
 
